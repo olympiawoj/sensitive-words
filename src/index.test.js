6df97bb77c7419aec71ee5c8e07f83316d74341f
olympiawoj <owojcik7@gmail.com>
2020-08-11 18:01:23 -0400

    Fix test in index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -10,6 +10,6 @@ test("replaces blacklisted words with astericks", () => {
 test("replaces multiple instances of blacklisted words with astericks",  () => {
     expect(
       sensitiveWords("The name of the NX will be the Nintendo Switch. The switch will be awesome.", ["switch"])
-    ).toBe("The name of the NX will be the Nintendo **** The **** will be awesome.");
+    ).toBe("The name of the NX will be the Nintendo ****. The **** will be awesome.");
   });
   
